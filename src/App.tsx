e2f899b75efb4ae1c62db977840bb76c950c44a5
davi <davi@sementedigital.com.br>
2021-03-25 20:00:13 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { ExperienceBar } from './styles/components/ExperienceBar'
+import './styles/global.css'
+
+function App() {
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  );
+}
+
+export default App;
